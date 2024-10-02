c4762b274dff7593549e8a4c51620784096d11d1
swarupa <swarupasolanke9@gmail.com>
2024-10-02 20:16:49 +0530

    translate using ngx translate

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,24 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import {HttpClient, provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
 import { routes } from './app.routes';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/', '.json');
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), 
+    importProvidersFrom([ TranslateModule.forRoot({
+    loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+    }, defaultLanguage: 'en'
+})]), 
+provideHttpClient(withInterceptorsFromDi())
+]
 };
